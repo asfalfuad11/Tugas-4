d0ce4f75bee421d82c1cda25cc31161cfc8ea518
Asfal Fuad <fuadasfal32@gmail.com>
2024-10-22 03:24:09 +0800

    Create perkalian_matriks.py

--- perkalian_matriks.py
+++ perkalian_matriks.py
@@ -1,33 +1,36 @@
-# Masukkan angka untuk matriks A dan B
+# Mendefinisikan matriks A dan B
 A = [
-    [1, 2, 3],
-    [4, 5, 6],
+    [-2, 2, 3],
+    [4, -6, 7],
     [7, 8, 9]
 ]
 
 B = [
     [9, 8, 7],
-    [6, 5, 4],
+    [6, -8, 4],
     [3, 2, 1]
 ]
 
-def multiply_matrices(A, B):
-    # Inisialisasi matriks hasil C
-    C = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
-    
-    # Melakukan perkalian matriks
-    for i in range(len(A)):
-        for j in range(len(B[0])):
-            for k in range(len(B)):
-                C[i][j] += A[i][k] * B[k][j]
-    
-    return C
+# Inisialisasi matriks hasil C
+C = [[0, 0, 0], 
+     [0, 0, 0], 
+     [0, 0, 0]]
 
-# Cek apakah matriks dapat dikalikan
-if len(A[0]) != len(B):
-    print("Perkalian matriks tidak dapat dilakukan!")
-else:
-    C = multiply_matrices(A, B)
-    print("Matriks Hasil C:")
-    for row in C:
-        print(row)
+# Melakukan perkalian matriks
+for i in range(len(A)):
+    for j in range(len(B[0])):
+        for k in range(len(B)):
+            C[i][j] += A[i][k] * B[k][j]
+
+# Menampilkan hasil
+print("Matriks A:")
+for row in A:
+    print(row)
+
+print("Matriks B:")
+for row in B:
+    print(row)
+
+print("Matriks Hasil C:")
+for row in C:
+    print(row)
